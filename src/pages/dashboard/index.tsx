681cf17caca7207454d06600c4fa2fb2d5de59a6
Anton Lindskog <lindskog96@live.se>
2024-04-22 11:37:54 +0200

    setup startpage

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,31 @@
-import React from "react";
 
-const dashboard = () => {
+import React from 'react'
+import { motion } from 'framer-motion';
+import styles from "./index.module.scss";
+
+const startPage = () => {
     return (
-        <h1>Dashboard</h1>
-    )
-}
+    <motion.div>
+        <div className={styles.headerContainer}>
+        <motion.div
+            initial={{ opacity: 0 }} 
+            animate={{ opacity: 1 }} 
+            transition={{ delay: 0.5 }}
+            className='header1'
+            >
+            <h1>Pints & Pattys</h1>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }} 
+            animate={{ opacity: 1 }} 
+            transition={{ delay: 1.8, duration: 3 }}
+            className={styles.header1}
+            >
+            <h2>The perfect pair, burger and beer</h2>
+          </motion.div>
+        </div>
+    </motion.div>
+      );
+    };
 
-export default dashboard;
+export default startPage;
