0335d97c1cb45bcfd8521caedf408c34d185032b
Anton Lindskog <lindskog96@live.se>
2024-04-22 10:44:34 +0200

    setup project

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const dashboard = () => {
-  return (
-    <h1 className='Header'>
-      Pints and pattys
-    </h1>
-  )
+    return (
+        <h1>Dashboard</h1>
+    )
 }
 
 export default dashboard;
