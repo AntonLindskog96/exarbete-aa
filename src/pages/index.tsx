d1f30c63af600dc471e14890a61a36b10d61f545
Anton Lindskog <lindskog96@live.se>
2024-04-22 14:59:45 +0200

    added styling, button, scroller

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,12 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.scss";
-import Dashboard from "./dashboard";
+import StartPage from "./dashboard";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Dashboard</title>
-        <meta name="description" content="Dashboard page" />
-      </Head>
       <main className={styles.main}>
-        <Dashboard />
+        <StartPage />
       </main>
     </>
   );
