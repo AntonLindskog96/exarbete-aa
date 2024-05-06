359041761ba8bfad0c08f37760c4ae2fe6325d70
Anton Lindskog <lindskog96@live.se>
2024-05-06 11:02:09 +0200

    added route to orderPage from button

---
+++ src/pages/orderPage/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import styles from './index.module.scss';
+
+const Orders: NextPage = () => {
+    return <h1 className={styles.orders}>Menu</h1>
+}
+
+export default Orders;
