5ca30172df73d19fbd22973ebd3d91e320702886
unknown <anton.haglund@iths.se>
2024-05-07 10:46:13 +0200

    Add logo, login and order

--- src/pages/header/header.tsx
+++ src/pages/header/header.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import homeIcon from "@/assets/images/home-icon.png";
+import styles from "@/pages/dashboard/index.module.scss";
+import {motion} from "framer-motion";
 
 const Header = () => {
     return (
-        <h1>
-            Header
-        </h1>
+        <div>
+            <img src={homeIcon.src} alt="icon" className={styles.icon}/>
+            <button className={styles.headerButton}>Beställ</button>
+            <button className={styles.loginButton}>Logga in</button>
+        </div>
     )
 }
 
