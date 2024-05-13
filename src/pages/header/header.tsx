f651595634b8b0848acd8e495cc0dc273657c69e
unknown <anton.haglund@iths.se>
2024-05-13 14:01:53 +0200

    Add hamburger menu with effects

--- src/pages/header/header.tsx
+++ src/pages/header/header.tsx
@@ -1,35 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import homeIcon from "@/assets/images/home-icon.png";
 import hamburgerMenuIcon from "@/assets/images/hamburger-menu.png";
 import styles from "@/pages/header/header.module.scss";
 
+
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    };
+
+
     return (
         <div className={styles.headerContent}>
             <div className={styles.iconContent}>
-                <img src={homeIcon.src} alt="icon" className={styles.icon}/>
+                <img src={homeIcon.src} alt="icon" className={styles.gg}/>
             </div>
             <div className={styles.buttonContent}>
                 <button className={styles.orderButton}>Beställ</button>
                 <button className={styles.loginButton}>Logga in</button>
             </div>
-            <div className={styles.hamburgerMenuContent}>
-                <img src={hamburgerMenuIcon.src} className= {styles.hamburgerMenuButton} height={40} width={40} />
+            <div className={styles.hamburgericon} onClick={toggleMenu}>
+                <img src={hamburgerMenuIcon.src} height={50}></img>
             </div>
             <nav id="nav">
 
-                <ul>
-                    <li>HOme</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                    <li>Help</li>
-                </ul>
+                {/*<ul>*/}
+                {/*    <li>HOme</li>*/}
+                {/*    <li>About</li>*/}
+                {/*    <li>Contact</li>*/}
+                {/*    <li>Help</li>*/}
+                {/*</ul>*/}
 
             </nav>
+            <div className={ ` ${styles["dark-blue"]} ${isMenuOpen ? styles.slide : ""}`}></div>
+            {isMenuOpen && <div className={styles.overlay} onClick={toggleMenu}></div> }
         </div>
 
 
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
