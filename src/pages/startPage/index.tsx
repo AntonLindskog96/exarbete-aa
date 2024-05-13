c7e89dd8372bd932fd4fcd3048d6989ffc5b2417
unknown <anton.haglund@iths.se>
2024-05-13 15:20:18 +0200

    Fix picture

--- src/pages/startPage/index.tsx
+++ src/pages/startPage/index.tsx
@@ -3,7 +3,7 @@ import {motion} from 'framer-motion';
 import styles from './index.module.scss';
 import Link from 'next/link';
 import Header from "@/pages/header/header";
-import burgerPicture from "@/assets/images/burger-picture.png";
+import burgerPicture from "@/assets/images/burger-picture.avif";
 
 const StartPage = () => {
 
