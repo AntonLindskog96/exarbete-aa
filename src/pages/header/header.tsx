449d4c73b9725727d711172bde4cde760e1b9d07
unknown <anton.haglund@iths.se>
2024-05-13 12:04:38 +0200

    Add hamburger menu

--- src/pages/header/header.tsx
+++ src/pages/header/header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import homeIcon from "@/assets/images/home-icon.png";
+import hamburgerMenuIcon from "@/assets/images/hamburger-menu.png";
 import styles from "@/pages/header/header.module.scss";
 
 const Header = () => {
@@ -12,7 +13,22 @@ const Header = () => {
                 <button className={styles.orderButton}>Beställ</button>
                 <button className={styles.loginButton}>Logga in</button>
             </div>
+            <div className={styles.hamburgerMenuContent}>
+                <img src={hamburgerMenuIcon.src} className= {styles.hamburgerMenuButton} height={40} width={40} />
+            </div>
+            <nav id="nav">
+
+                <ul>
+                    <li>HOme</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Help</li>
+                </ul>
+
+            </nav>
         </div>
+
+
     )
 }
 
