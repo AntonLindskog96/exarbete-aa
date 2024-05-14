0fe72f9f833f85dfd4014a32086e6ea668941416
unknown <anton.haglund@iths.se>
2024-05-14 11:49:12 +0200

    Add login startup module

---
+++ src/modules/login.tsx
@@ -0,0 +1,10 @@
+import styles from "@/styles/Home.module.scss";
+import StartPage from "@/pages/startPage";
+
+export default function Login() {
+    return (
+        <div>
+            <h2>Login</h2>
+        </div>
+    );
+}
